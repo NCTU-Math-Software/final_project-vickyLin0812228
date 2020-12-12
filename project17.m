fe7fdac20f335d0f90cc034f17cb2dba4209daa3
vickyLin0812228 <71300670+vickyLin0812228@users.noreply.github.com>
2020-12-12 14:36:51 +0800

    Update project17.m

--- project17.m
+++ project17.m
@@ -1,43 +1,77 @@
-n=5;
-value=2;
-A=[];
-B=[];
-for ii=1:n
-    for jj=1:n
-        A(ii,jj)=0;
+function matrixClockwise(n)
+    value=2;
+    A=[];
+    B=[];
+    for ii=1:n
+        for jj=1:n
+            A(ii,jj)=0;
+        end
     end
-end
-N=(n+1)/2;
-A(N,N)=1;
+    
+    if mod(n,2)==0
+        N=n/2;
+        A(N,N)=1;
 
-for ii=1:n
-    if mod(ii,2)==0
-        for jj=1:ii
-            A(N,N-jj)=value;
-            value=value+1;
+        for ii=1:n
+            if mod(ii,2)==0
+                for jj=1:ii
+                    if N-jj~=0
+                        A(N,N-jj)=value;
+                        value=value+1;
+                    end
+                end
+                for jj=1:ii
+                    if (N-ii~=0)&&(N-jj~=0)
+                        A(N-jj,N-ii)=value;
+                        value=value+1;
+                    end
+                end
+                N=N-ii;
+            else
+                for jj=1:ii
+                    A(N,N+jj)=value;
+                    value=value+1;    
+                end
+                for jj=1:ii
+                    A(N+jj,N+ii)=value;
+                    value=value+1; 
+                end
+                N=N+ii;
+            end
         end
-        for jj=1:ii
-            A(N-jj,N-ii)=value;
-            value=value+1;
-        end
-        N=N-ii;
     else
-        for jj=1:ii
-            A(N,N+jj)=value;
-            value=value+1;    
-        end
-        for jj=1:ii
-            A(N+jj,N+ii)=value;
-            value=value+1; 
-        end
-        N=N+ii;
-    end
-end
+        N=(n+1)/2;
+        A(N,N)=1;
 
-for ii=1:n
-    for jj=1:n
-        B(ii,jj)=A(ii,jj);
+        for ii=1:n
+            if mod(ii,2)==0
+                for jj=1:ii
+                    A(N,N-jj)=value;
+                    value=value+1;
+                end
+                for jj=1:ii
+                    A(N-jj,N-ii)=value;
+                    value=value+1;
+                end
+                N=N-ii;
+            else
+                for jj=1:ii
+                    A(N,N+jj)=value;
+                    value=value+1;    
+                end
+                for jj=1:ii
+                    A(N+jj,N+ii)=value;
+                    value=value+1; 
+                end
+                N=N+ii;
+            end
+        end
     end
+    
+    for ii=1:n
+        for jj=1:n
+            B(ii,jj)=A(ii,jj);
+        end
+    end
+    disp(B) 
 end
-
-disp(B)
