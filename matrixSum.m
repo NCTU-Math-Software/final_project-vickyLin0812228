ea4882a3c6057c4a369628dd4c2b173018baeae6
vickyLin0812228 <71300670+vickyLin0812228@users.noreply.github.com>
2020-12-18 01:39:45 +0800

    Update matrixSum.m

--- matrixSum.m
+++ matrixSum.m
@@ -9,23 +9,23 @@ function matrixSum(n)
     %---------------------------------------------------------------------------------
     % 當 n 為奇數時執行
     if mod(n,2)~=0
-        sum=1;                  % 設定總和初始值
-        if n==1                 % n=1 時執行
-            disp(sum)
+        sum=1;                                % 設定總和初始值
+        if n==1                               % n=1 時執行
+            disp(['Sum = ', num2str(sum)])    % 顯示結果
         else
             % 計算總和
             for ii=2:(n+1)/2
                 sum=sum+(2*ii-1-2)^2*4+(2*ii-1-1)*10;
             end
-            sum=sum+1;          % 因為 1 重複算 2 次
-            disp(sum)           % 顯示結果
+            sum=sum+1;                        % 因為 1 重複算 2 次
+            disp(['Sum = ', num2str(sum)])    % 顯示結果
         end 
     else
-        sum=0;                  % 設定總和初始值
+        sum=0;                                % 設定總和初始值
         % 計算總和
         for ii=1:n/2
             sum=sum+(2*ii-2)^2*4+(2*ii-1)*10;
         end
-        disp(sum)               % 顯示結果
+        disp(['Sum = ', num2str(sum)])        % 顯示結果
     end
 end
