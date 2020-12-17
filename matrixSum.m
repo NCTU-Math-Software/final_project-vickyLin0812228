4030a9673caae3062470c29e56c0a5bbb7dbf2e9
vickyLin0812228 <71300670+vickyLin0812228@users.noreply.github.com>
2020-12-18 01:34:20 +0800

    Update matrixSum.m

--- matrixSum.m
+++ matrixSum.m
@@ -1,20 +1,31 @@
 function matrixSum(n)
+    % 給定 n×n 的此種矩陣, 求其對角線 (a_i,i) 及反對角線 (a_n−i,i) 之總和
+    % Input: n, an integer, 矩陣大小
+    % Output: sum, an integer, 其對角線 (a_i,i) 及反對角線 (a_n−i,i) 之總和
+    % 
+    % example: 
+    % >> n=5;
+    % >> matrixSum(n)
+    %---------------------------------------------------------------------------------
+    % 當 n 為奇數時執行
     if mod(n,2)~=0
-        sum=1;
-        if n==1
+        sum=1;                  % 設定總和初始值
+        if n==1                 % n=1 時執行
             disp(sum)
         else
+            % 計算總和
             for ii=2:(n+1)/2
                 sum=sum+(2*ii-1-2)^2*4+(2*ii-1-1)*10;
             end
-            sum=sum+1;
-            disp(sum)
+            sum=sum+1;          % 因為 1 重複算 2 次
+            disp(sum)           % 顯示結果
         end 
     else
-        sum=0;
+        sum=0;                  % 設定總和初始值
+        % 計算總和
         for ii=1:n/2
             sum=sum+(2*ii-2)^2*4+(2*ii-1)*10;
         end
-        disp(sum)
+        disp(sum)               % 顯示結果
     end
 end
